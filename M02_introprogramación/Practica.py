8727f6d74b706da67941f8dbfe9951059db07f07
root <root@localhost.localdomain>
2025-04-27 20:40:11 +0000

    Primer commit del proyecto

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -8,3 +8,21 @@ a = 20
 while a > 0:
     print(a)
     a -= 1
+
+print("hola soyhenry")
+a = 100
+while a > 0:
+     print(a)
+     a -= 2
+
+print("alegre")
+a = 10000
+while a > 0:
+     print(a)
+     a -= 2
+
+print("madre")
+a = 1000000
+while a > 0:
+      print(a)
+      a -= 2
